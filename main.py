09c5353076bc263aff222c82472bebf8f783f796
lusm <loveyousomuch554@gmail.com>
2024-05-13 22:24:03 +0300

    log to file and catch errors

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from pynput import keyboard
 import logging
+import zoneinfo # added since python 3.9
+import datetime
 
+logging.Formatter.converter = lambda *args: datetime.datetime.now(tz=zoneinfo.ZoneInfo("Europe/Moscow")).timetuple()
 logging.basicConfig(
   level=logging.INFO,
-  format='[%(asctime)s] %(levelname)s [%(name)s] %(message)s',
+  format='[%(asctime)s] %(levelname)s [%(name)] %(message)s',
   datefmt='%Y-%m-%d %H:%M:%S %Z',
+  handlers=[
+    logging.FileHandler(datetime.datetime.now().strftime('logs/log_%Y-%m-%d_%H-%M-%S.log')),
+    logging.StreamHandler(),
+  ]
 )
 logger = logging.getLogger(__name__)
+
+def save_result():
+  pass
 
 def on_press(key):
   try:
@@ -17,16 +27,19 @@ def on_press(key):
 def on_release(key):
   logger.info(f'Key {key} released')
   if key == keyboard.Key.esc:
+    raise StopIteration
     # Stop listener
-    return False
+    #return False
 
 # Collect events until released
-with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-  listener.join()
+
 
 # ...or, in a non-blocking fashion:
-listener = keyboard.Listener(
-  on_press=on_press,
-  on_release=on_release
-) 
-listener.start()
+def main():
+  try:
+    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+      listener.join()
+  except:
+    logger.info('before exit')
+
+main()
